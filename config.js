b1f4e7936c039b58d6702f0b7ff2b7e787ac970e
Ipmake <60793254+Ipmake@users.noreply.github.com>
2023-05-28 23:40:36 +0200

    Convert maxPlayers to a number if it isnt already one

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 const config = require("./config.json");
 
+const maxPlayers = process.env.MAX_PLAYERS || config.maxPlayers
+
 module.exports = {
   name: process.env.SERVER_NAME || config.name,
   publicAddr: process.env.PUBLIC_ADDRESS || config.publicAddr,
-  maxPlayers: process.env.MAX_PLAYERS || config.maxPlayers,
+  maxPlayers: typeof maxPlayers != "number" ? parseInt(maxPlayers) : maxPlayers,
   external: {
     port: process.env.EXTERNAL_PORT || config.external.port,
   },
